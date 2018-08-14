8f418c4a81cc6734719836be581c8d5bec4d8282
Robert Koeninger <rkoeninger@att.net>
2018-08-13 22:26:13 -0400

    Fixed Hm module, added dune-workspace

--- lib/hm.ml
+++ lib/hm.ml
@@ -1,9 +1,18 @@
-module HindleyMilner =
-  struct
+type syn_expr =
+  | Var of string
+  | App of syn_expr * syn_expr list
+  | Abs of string * syn_expr
+  | Let of string * syn_expr * syn_expr
+  | Ann of syn_type * syn_expr
 
-    (* let infer_type expr = ""
-    let check_type expr = true *)
+and syn_type = string
 
-    let f x = x
+let check_type _ _ = true
 
-  end
+let infer_type expr =
+  match expr with
+  | Var _ -> Some "var"
+  | App(_, _) -> Some "app"
+  | Abs(_, _) -> Some "abs"
+  | Let(_, _, _) -> Some "let"
+  | Ann(t, _) -> Some t
--- lib/hm.mli
+++ lib/hm.mli
@@ -1,18 +1,5 @@
-module HindleyMilner :
-  sig
+type syn_expr
+type syn_type
 
-    (* type syn_type = string
-
-    type syn_expr =
-      | Var of string
-      | App of syn_expr * syn_expr list
-      | Abs of string * syn_expr
-      | Let of string * syn_expr * syn_expr
-      | Ann of syn_type * syn_expr
-
-    val infer_type : syn_expr -> syn_type
-    val check_type : syn_expr -> bool *)
-
-    val f : int -> int
-
-  end
+val check_type : syn_type -> syn_expr -> bool
+val infer_type : syn_expr -> syn_type option
